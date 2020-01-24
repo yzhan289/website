ae63d5e039d53a1066fb005805cde582ffd6afb8
Andrew Zhang <31313038+yzhan289@users.noreply.github.com>
2020-01-24 16:14:44 -0500

    made build folder

---
+++ build/precache-manifest.e274ce9b7633e6c8c736b3e3ea07a736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d11a4afa632576697ce197d1d4710e4",
+    "url": "/yzhan289.github.io/index.html"
+  },
+  {
+    "revision": "a12e3b98c5b8c7ce7fdf",
+    "url": "/yzhan289.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3c14714de6f8372e63ee",
+    "url": "/yzhan289.github.io/static/js/2.333e62d4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/yzhan289.github.io/static/js/2.333e62d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a12e3b98c5b8c7ce7fdf",
+    "url": "/yzhan289.github.io/static/js/main.486d940f.chunk.js"
+  },
+  {
+    "revision": "5f5740e1e03e255a14cf",
+    "url": "/yzhan289.github.io/static/js/runtime-main.bd5c1b4c.js"
+  }
+]);
